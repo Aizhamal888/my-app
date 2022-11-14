7f58fc6d2b9dc065a5a4cf1a894fd193edbb63ec
AMamasydykova <aizhamalka.m@mail.com>
2022-11-14 17:30:45 +0600

    init: start

---
+++ src/components/IntroductionMenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import About from "./About";
+import PopularPosts from "./PopularPosts";
+import Tags from "./Tags";
+
+
+const IntroductionMenu = () => {
+    return (
+        <>
+
+            <div className="w3-col l4">
+                <About/>
+                <PopularPosts/>
+                <Tags/>
+
+            </div>
+        </>
+
+    )
+}
+export default IntroductionMenu;
